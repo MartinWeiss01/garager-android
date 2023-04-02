5d8b892d58dc9cb327b2e5a56989410e8a1093e8
Martin Weiss <jsem@martinweiss.cz>
2023-04-03 01:06:59 +0200

    refactor: remove unused pkgs

--- Garager/app/src/main/java/cz/martinweiss/garager/MainActivity.kt
+++ Garager/app/src/main/java/cz/martinweiss/garager/MainActivity.kt
@@ -1,28 +1,14 @@
 package cz.martinweiss.garager
 
-import android.app.DatePickerDialog
 import android.os.Bundle
-import android.widget.DatePicker
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import cz.martinweiss.garager.navigation.Destination
 import cz.martinweiss.garager.navigation.NavGraph
-import cz.martinweiss.garager.ui.screens.vehicleList.VehicleListScreen
 import cz.martinweiss.garager.ui.theme.GaragerTheme
-import java.time.LocalDate
-import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
