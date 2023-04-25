ea820b763b93b070919287eab6877a750105ff24
Martin Weiss <jsem@martinweiss.cz>
2023-04-25 14:15:21 +0200

    ci(AddEditVeh): add file picker button
    Temporary solution to see behavior on different Android versions (tested on Droid 9/API 28 & Droid 12/API 31)

--- Garager/app/src/main/java/cz/martinweiss/garager/ui/screens/vehicleAddEdit/Screen.kt
+++ Garager/app/src/main/java/cz/martinweiss/garager/ui/screens/vehicleAddEdit/Screen.kt
@@ -69,6 +69,7 @@ fun AddEditVehicleContent(
     actions: AddEditVehicleViewModel,
     data: AddEditVehicleData
 ) {
+    Text(text = "${Build.VERSION.SDK_INT} -- ${Build.VERSION_CODES.Q}")
     var expanded by remember { mutableStateOf(false) }
 
     Column(
@@ -130,6 +131,8 @@ fun AddEditVehicleContent(
             }
         }
 
+        FilePickerDev()
+
         Row(
             Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.Center
@@ -142,6 +145,69 @@ fun AddEditVehicleContent(
             ) {
                 Text(text = stringResource(id = R.string.add_edit_vehicle_save_btn))
             }
+        }
+    }
+}
+
+@Composable
+fun FilePickerDev() {
+    val context = LocalContext.current
+    val result = remember { mutableStateOf<Uri?>(null) }
+
+    var bitmap by remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+
+    // Android 10+ (API Level 29+), permissions not required
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.OpenDocument()) {
+        result.value = it
+        if(it == null) {
+            Log.d("[ ################### ]", "Selected URI is null")
+        } else {
+            var fileContent = context.contentResolver.openInputStream(it)?.readBytes()
+            bitmap = fileContent?.let { BitmapFactory.decodeByteArray(fileContent, 0, it.size) }
+            Log.d("[ ################### ]", it.toString())
+            //Log.d("[ ################### ]", fileContent.toString())
+        }
+    }
+
+    // Android <10 (API Level <29), permissions required
+    val permissionLauncher = rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            launcher.launch(arrayOf("application/pdf", "image/*"))
+        } else {
+            Toast.makeText(context, "Oprávnění bylo zamítnuto", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    Column() {
+        Text(text = stringResource(id = R.string.add_edit_vehicle_file_field))
+        Button(onClick = {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            } else {
+                launcher.launch(arrayOf("application/pdf", "image/*"))
+            }
+        }) {
+            Text(text = stringResource(id = R.string.add_edit_vehicle_upload_file))
+        }
+    }
+
+    var scale by remember { mutableStateOf(1f) }
+    val state = rememberTransformableState { zoomChange, _, _ ->
+        scale *= zoomChange
+    }
+
+    if(bitmap != null) {
+        Box(
+            Modifier
+                .transformable(state = state)
+        ) {
+            Image(
+                bitmap!!.asImageBitmap(),
+                contentDescription = null,
+                modifier = Modifier.scale(scale)
+            )
         }
     }
 }
