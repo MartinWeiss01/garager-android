2d31d6f50604260dd8ef4fd14f2bf21afefc1bdd
Martin Weiss <jsem@martinweiss.cz>
2023-04-27 00:19:20 +0200

    ci(elements): add support for action btn on BAC

--- Garager/app/src/main/java/cz/martinweiss/garager/ui/elements/BackArrowScreen.kt
+++ Garager/app/src/main/java/cz/martinweiss/garager/ui/elements/BackArrowScreen.kt
@@ -5,16 +5,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BackArrowScreen(
     topBarTitle: String,
     onBackClick: () -> Unit,
-    content: @Composable (paddingValues: PaddingValues) -> Unit
+    actionIcon: ImageVector? = null,
+    onActionClick: (() -> Unit)? = null,
+    content: @Composable (paddingValues: PaddingValues) -> Unit,
 ) {
     Scaffold(
         topBar = {
@@ -23,6 +27,18 @@ fun BackArrowScreen(
                 navigationIcon = {
                     IconButton(onClick = { onBackClick() } ) {
                         Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "")
+                    }
+                },
+                actions = {
+                    if(actionIcon != null && onActionClick != null) {
+                        IconButton(onClick = {
+                            onActionClick()
+                        }) {
+                            Icon(
+                                imageVector = actionIcon,
+                                contentDescription = null
+                            )
+                        }
                     }
                 }
             )
