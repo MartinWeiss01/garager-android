3ef49d3d2e32a6fbb973531e773f6ef66892bee1
Martin Weiss <jsem@martinweiss.cz>
2023-04-29 00:42:19 +0200

    ci(Settings): prepare MOT expiry warn picker
    Using 3rd party Number Picker

--- Garager/app/src/main/java/cz/martinweiss/garager/ui/screens/settings/Screen.kt
+++ Garager/app/src/main/java/cz/martinweiss/garager/ui/screens/settings/Screen.kt
@@ -1,11 +1,15 @@
 package cz.martinweiss.garager.ui.screens.settings
 
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
+import android.os.Build
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.chargemap.compose.numberpicker.NumberPicker
 import cz.martinweiss.garager.navigation.INavigationRouter
 import cz.martinweiss.garager.R
 import cz.martinweiss.garager.ui.elements.BottomNavigationBar
@@ -18,7 +22,7 @@ fun SettingsScreen(navigation: INavigationRouter) {
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
-                    Text(text = stringResource(id = R.string.title_settings_screen)
+                    Text(text = stringResource(id = R.string.app_name)
                         .uppercase(Locale.getDefault())
                     )
                 },
@@ -39,6 +43,53 @@ fun SettingsScreenContent(
     Surface(
         modifier = Modifier.padding(paddingValues)
     ) {
-        Text(text = "Settings Screen")
+        Column(
+            modifier = Modifier.padding(20.dp),
+            verticalArrangement = Arrangement.spacedBy(40.dp)
+        ) {
+            Text(text = stringResource(id = R.string.title_settings_screen))
+            Text(text = "Build: ${Build.VERSION.SDK_INT} | Q: ${Build.VERSION_CODES.Q}")
+            MOTExpirationDays()
+        }
     }
+}
+@Composable
+fun MOTExpirationDays() {
+    var pickerValue by remember { mutableStateOf(0) }
+
+    SettingsElement {
+        Column(modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Center) {
+            Text(text = stringResource(id = R.string.settings_mot_days_warning_label))
+            Text(text = stringResource(id = R.string.settings_mot_days_warning_description), fontSize = 10.sp, lineHeight = 15.sp)
+        }
+
+        Row(
+            modifier = Modifier.padding(start = 10.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            NumberPicker(
+                value = pickerValue,
+                range = 10..60,
+                onValueChange = {
+                    pickerValue = it
+                }
+            )
+
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Text(text = stringResource(id = R.string.settings_mot_days_warning_days))
+        }
+    }
+}
+
+@Composable
+fun SettingsElement(
+    content: @Composable RowScope.() -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        content = content
+    )
 }
