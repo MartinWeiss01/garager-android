53811e2d6fca1ec8cc0446a21d5c8eac60383d45
Martin Weiss <jsem@martinweiss.cz>
2023-04-30 16:08:54 +0200

    ci(utils): add TimeDate support to DateUtils
    getDateString can now return only date or timedate

--- Garager/app/src/main/java/cz/martinweiss/garager/utils/DateUtils.kt
+++ Garager/app/src/main/java/cz/martinweiss/garager/utils/DateUtils.kt
@@ -7,16 +7,20 @@ import java.util.concurrent.TimeUnit
 class DateUtils {
     companion object {
         private val DATE_FORMAT_CS = "dd. MM. yyyy"
+        private val TIMEDATE_FORMAT_CS = "dd. MM. yyyy HH:mm"
         private val DATE_FORMAT_EN = "yyyy/MM/dd"
+        private val TIMEDATE_FORMAT_EN = "yyyy/MM/dd HH:mm"
 
-        fun getDateString(unixTime: Long): String{
+        fun getDateString(unixTime: Long, includeTime: Boolean = false): String{
             val calendar = Calendar.getInstance()
             calendar.timeInMillis = unixTime
             val format: SimpleDateFormat
             if (LanguageUtils.isLanguageCzech()){
-                format = SimpleDateFormat(DATE_FORMAT_CS, Locale.GERMAN)
+                format = if(includeTime) SimpleDateFormat(TIMEDATE_FORMAT_CS, Locale.GERMAN)
+                else SimpleDateFormat(DATE_FORMAT_CS, Locale.GERMAN)
             } else {
-                format = SimpleDateFormat(DATE_FORMAT_EN, Locale.ENGLISH)
+                format = if(includeTime) SimpleDateFormat(TIMEDATE_FORMAT_EN, Locale.ENGLISH)
+                else SimpleDateFormat(DATE_FORMAT_EN, Locale.ENGLISH)
             }
             return format.format(calendar.getTime())
         }
