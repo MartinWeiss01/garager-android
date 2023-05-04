a1de1891750b47a0a1ebc8cfd0636dc510d53d5f
Martin Weiss <jsem@martinweiss.cz>
2023-05-04 17:58:20 +0200

    fix(db): cascade fuelings delete on vehicle delete

--- Garager/app/src/main/java/cz/martinweiss/garager/model/Fueling.kt
+++ Garager/app/src/main/java/cz/martinweiss/garager/model/Fueling.kt
@@ -8,7 +8,8 @@ import androidx.room.*
         ForeignKey(
             entity = Vehicle::class,
             parentColumns = ["id"],
-            childColumns = ["vehicle_id"]
+            childColumns = ["vehicle_id"],
+            onDelete = ForeignKey.CASCADE
         )
     ]
 )
