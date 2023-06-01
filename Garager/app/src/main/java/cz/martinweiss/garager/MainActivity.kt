73aab5362c98c195960a006ef546e29a939664a0
Martin Weiss <jsem@martinweiss.cz>
2023-06-01 18:53:40 +0200

    feat(Splash): add SplashScreen with branding

--- Garager/app/src/main/java/cz/martinweiss/garager/MainActivity.kt
+++ Garager/app/src/main/java/cz/martinweiss/garager/MainActivity.kt
@@ -7,13 +7,27 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import cz.martinweiss.garager.navigation.Destination
 import cz.martinweiss.garager.navigation.NavGraph
 import cz.martinweiss.garager.ui.theme.GaragerTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
+        /*
+        var keepSplashScreenVisible = true
+        splashScreen.setKeepOnScreenCondition { keepSplashScreenVisible }
+        lifecycleScope.launch {
+            delay(3000)
+            keepSplashScreenVisible = false
+        }
+        */
+
         setContent {
             GaragerTheme {
                 // A surface container using the 'background' color from the theme
